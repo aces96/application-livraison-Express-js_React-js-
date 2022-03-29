7121d871b3fffef9a6fb6411d39362af9b3d4d7d
aces96 <esraidi12@gmail.com>
2022-03-29 15:39:47 +0000

    i made some modification in authentification functions

--- utils/authentificationUtils/tokenHandler.js
+++ utils/authentificationUtils/tokenHandler.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 
 const tokenGen = (id)=>{
-    return jwt.sign({id,role}, process.env.SECRET_KEY,{expiresIn: process.env.EXPIRE_IN})
+    return jwt.sign({id: id}, process.env.SECRET_KEY,{expiresIn: process.env.EXPIRE_IN})
 }
 
 
